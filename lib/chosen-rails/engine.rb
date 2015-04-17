23a6681782351b4b2f5642461170935ca1e461cf
omc10 <omc10@ws-omc10.omc.net>
2015-04-17 14:06:49 +0200

    made engine usable with recent rails versions

--- lib/chosen-rails/engine.rb
+++ lib/chosen-rails/engine.rb
@@ -1,9 +1,9 @@
 module Chosen
   module Rails
     class Engine < ::Rails::Engine
-      config.assets.precompile += %w(
-        chosen-sprite*.png
-      )
+      initializer :assets do |config|
+       Rails.application.config.assets.precompile += %w{ chosen-sprite*.png }
+      end
 
       rake_tasks do
         load 'chosen-rails/tasks.rake'
